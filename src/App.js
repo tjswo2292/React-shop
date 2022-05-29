fc4ec320ad9287ec6fd1b7cbc16f0ff223513be4
tjswo2292 <tjswo2292@naver.com>
2022-05-29 17:28:08 +0900

    get Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import './App.css';
 
+import { useEffect, useState } from 'react';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
 
 import data from './data.js';
@@ -12,7 +13,18 @@ import Detail from './routes/Detail';
 
 function App() {
 
+  useEffect(() =>{
+    axios.get('https://codingapple1.github.io/shop/data2.json')
+    .then((response) => {
+      setGetdata(response.data);
+    })
+    .catch(() => {
+      alert('Error');
+    })
+  }, [])
+
   let [shoes] = useState(data);
+  let [getData, setGetdata] = useState();
   let navigate = useNavigate();
 
   return (
@@ -31,7 +43,7 @@ function App() {
       <Route path='/' element={
         <>
         <div className='main-bg'></div>
-          <div className='container'>
+        <div className='container'>
           <div className='row'>
             {
               shoes.map((a, i) => {
@@ -41,7 +53,25 @@ function App() {
               })
             }
           </div>
+          <div>
+            {
+              getData.map((a, i) => {
+                return (
+                  <>
+                    <span>{getData[i].title}</span>
+                    <span>{getData[i].content}</span>
+                    <span>{getData[i].price}</span>
+                  </>
+                )
+              })
+            }
+          </div>
         </div>
+        {/* <button onClick={()=>{
+          axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then((result) => {console.log(result.data)})
+          .catch(()=>{console.log('요청실패')})
+        }}>버튼</button> */}
         </>
       }/>
 
@@ -58,3 +88,5 @@ function App() {
 export default App;
 
 
+// 데이터 가져와서 html로 보여주기
+// mock 서버, fake 서버, reqres
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,11 +3,17 @@ import { useParams } from "react-router-dom";
 
 
 function Detail({ shoes }) {
-    let [alert ,setAlert] = useState(true);
-    // mount, update 때 실행이 됨
     useEffect(() => {
-        setTimeout(() => {setAlert(false)}, 2000);
-    })
+        // setTimeout(() => {setAlert(false)}, 2000)
+        
+        // 숫자가 아닐 경우 true 반환
+        // if(isNaN(inputValue) === true){
+        //     alert('그러지마');
+        // }
+    }, []) // 1회만 동작하기 원하면 [] 넣기 
+
+    // let [alert, setAlert] = useState();
+    let [inputValue, setInputvalue] = useState('');
     
     let {id} = useParams();
     let findPro = shoes.find(x => x.id == id);
@@ -25,11 +31,12 @@ function Detail({ shoes }) {
                 <button className="btn btn-danger">주문하기</button> 
             </div>
         </div>
-        {
+        {/* {
             alert === true ?
-            <div style={{backgroundColor: 'yellow'}}>2초 안에 누르면 구매 쌉가능</div>
+            <div style={{backgroundColor: 'yellow'}}>2초 동안 구매 가능</div>
             : null
-        }
+        } */}
+        {/* <input onChange={(e) =>{setInputvalue(e.target.value)}}/> */}
     </div>
     )
 }
@@ -39,3 +46,5 @@ export default Detail;
 // 상품 id 값으로 상품 보여주기
 // Detail 페이지에 노란색 박스를 만들고, 2초 후에 사라지게 하기
 // 동적인 ui는 useState로 관리하고, 2초 후에 사라지게 하는 조건문
+
+// input에 숫자말고 다른거 입력하면 그러지말라고 안내 메세지 띄우기
